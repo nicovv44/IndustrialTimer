61577131fb8d5b3bca204027493c8d8d08a90c3f
Nicolas VERHELST <nicolas.nv.verhelst@gmail.com>
2021-02-16 08:46:32 +1100

    Rename global variables and put them as static

--- My Project LCD2/Timer/CustomTimer.c
+++ My Project LCD2/Timer/CustomTimer.c
@@ -10,14 +10,15 @@
 #include "LCD/lcd_drv.h"
 #include <stdbool.h>
 
-functionTypeAction GlobalAction;
-long GlobalTargetCount = 1;
-bool RepeatAction = false;
+// Global variable to be accessed within this file
+static functionTypeAction G_Action;
+static long G_TargetCount = 1;
+static bool G_RepeatAction = false;
 
 void actionWrapper()
 {
-	GlobalAction();
-	if (!RepeatAction)
+	G_Action();
+	if (!G_RepeatAction)
 	{
 		timer1_stop();
 	}
@@ -27,7 +28,7 @@ void waitUntilAction()
 {
 	static long count = 0;
 	count++;
-	if (count==GlobalTargetCount)
+	if (count==G_TargetCount)
 	{
 		count = 0;
 		actionWrapper();
@@ -38,8 +39,8 @@ void waitUntilAction()
 
 void startTimerAction(long timeMs, bool repeat, void (*action)())
 {
-	RepeatAction = repeat;
-	GlobalAction = action;
+	G_RepeatAction = repeat;
+	G_Action = action;
 	if(timeMs<=4)
 	{
 		int ticksToWait = 65535/4*timeMs;
@@ -47,7 +48,7 @@ void startTimerAction(long timeMs, bool repeat, void (*action)())
 	}
 	else
 	{
-		GlobalTargetCount = timeMs/4;
+		G_TargetCount = timeMs/4;
 		timer1(TIMER1_PRESCALER_1, 65535, waitUntilAction);
 	}
 }
