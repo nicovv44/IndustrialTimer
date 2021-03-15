7fffd992c2925d6daa0ecfe3837bb1910277d3bf
Nicolas VERHELST <nicolas.nv.verhelst@gmail.com>
2021-03-15 20:55:13 +1100

    Go from 16MHz crystalto 8MHz crystal (hardware limit of ATMEGA328PB)

--- My Project DigitalTimer/Config/clock_config.h
+++ My Project DigitalTimer/Config/clock_config.h
@@ -5,7 +5,8 @@
 // <<< Use Configuration Wizard in Context Menu >>>
 
 #ifndef F_CPU
-#define F_CPU 16000000
+//#define F_CPU 1000000 // 8MHz/8 (because CKDIV8 with 8MHz crystal)
+#define F_CPU 8000000 // 8MHz/1 (because no CKDIV8 with 8MHz crystal)
 #endif
 
 // <<< end of configuration section >>>
